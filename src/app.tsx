64fcd76569182390ce43c6ad61cf5c9546f9bc31
Anthony Lee <hososlch@gmail.com>
2022-06-20 13:22:08 +0800

    fix: build

--- src/app.tsx
+++ src/app.tsx
@@ -3,21 +3,6 @@ import {UserInfoEditPage} from "pages/user-info";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 export const App = React.memo(() => {
-    // React.useEffect(() => {
-    //     Telegram.WebApp.ready();
-    //     Telegram.WebApp.MainButton.isVisible = true;
-    //     Telegram.WebApp.MainButton.text = "Clicked!";
-
-    //     Telegram.WebApp.onEvent("mainButtonClicked", () => {
-    //         Telegram.WebApp.MainButton.text = "Clicked!";
-
-    //         try {
-    //             Telegram.WebApp.sendData(JSON.stringify({fuck: 1}));
-    //             Telegram.WebApp.close();
-    //         } catch (e) {}
-    //     });
-    // }, []);
-
     return (
         <BrowserRouter>
             <Routes>
